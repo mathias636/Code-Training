fafc4ec493a795c0c1fe4e469846069a6ada1045
mathias636 <64647213+mathias636@users.noreply.github.com>
2021-11-07 09:57:08 -0300

    1001.java - Incomplete

---
+++ Beginner-Java/src/1001.java
@@ -0,0 +1,15 @@
+import java.util.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+		Scanner scanner = new Scanner (system.in);
+		int A;
+		int B;
+		int X;
+		A = scanner.nextInt();
+		B = scanner.nextInt();
+		X = A + B;
+		System.out.println("X = "+X);
+	}
+}
