8b1e3eccc2485e323a40ff5b0e34326f9396adc0
mathias636 <64647213+mathias636@users.noreply.github.com>
2021-11-22 22:29:20 -0300

    Array exercises

---
+++ Beginner-Java/src/problem1172/Main.java
@@ -0,0 +1,49 @@
+﻿package problem1172;
+/*
+    problem 1172 URI
+
+    Read an array X[10]. After, replace every null or negative number of X ​by 1. Print all numbers stored in the array X.
+
+    Input
+    The input contains 10 integer numbers. These numbers ​​can be positive or negative.
+
+    Output
+    For each position of the array, print "X [i] = x", where i is the position of the array and x is the number stored in that position.
+
+    Input Sample
+    0
+    -5
+    63
+    0
+    ...
+
+    Output Sample
+    
+    X[0] = 1
+    X[1] = 1
+    X[2] = 63
+    X[3] = 1
+    ...
+
+    Thanks to Cassio F.
+*/
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int l = 10;
+        int X[] = new int[l];
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i < l; i++){
+            X[i] = scanner.nextInt();
+            if(X[i] == 0 || X[i] < 0){
+                X[i] = 1;
+            }
+        }
+        for(int i = 0; i < l; i++){
+            System.out.println("X["+i+"] = "+X[i]);
+        }
+        scanner.close();
+    }
+}
---
+++ Beginner-Java/src/problem1173/Main.java
@@ -0,0 +1,44 @@
+﻿package problem1173;
+/*
+Read a number and make a program which puts this number in the first position of an array N[10].
+In each subsequent position, put the double of the previous position. 
+For example, if the input number is 1, the array numbers ​​must be 1,2,4,8, and so on.
+
+Input
+The input contains an integer number V (V < 50).
+
+Output
+Print the stored number of each array position, in the form "N[i] = X", 
+where i is the position of the array and x is the stored number at the position i. The first number for X is V.
+
+Input Sample
+1
+
+Output Sample
+N[0] = 1
+N[1] = 2
+N[2] = 4
+...
+
+Thanks to Cassio F.
+*/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int V;
+        int l = 10;
+        int N[] = new int[l];
+        do
+        {
+            V = input.nextInt();
+        }while (V >= 50);
+        for(int i = 0; i < l; i++){
+            N[i] = V;
+            System.out.println("N["+i+"] = "+V);
+            V = V*2;
+        }
+        input.close();
+    }
+}
